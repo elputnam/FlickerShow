05b81a637366f9e1d48b8079f544c66531ead24a
EL Putnam <elputnam@inaction.ie>
2023-07-05 19:48:52 +0100

    add flickering and fading dad; color tone EL

--- sketch.js
+++ sketch.js
@@ -6,11 +6,16 @@ let EL;
 let x = 0;
 let y = 0;
 let num;
+let alp = 0;
+let MIN = 0;
+let MAX = 100;
+let g  = 0.5;
 
 //preload assets: base photo, EL, 9 squares (in array)
 function preload(){
   base = loadImage('assets/ELDad_books_base.png');
-  EL = loadImage('assets/ELDad_books_EL.png')
+  EL = loadImage('assets/ELDad_books_EL.png');
+  dad = loadImage('assets/ELDad_books_dad.png')
   for (let i = 0; i < 9; i++){
     squares[i] = loadImage("assets/Grid_" + i + ".png");
   }  
@@ -24,7 +29,8 @@ function setup() {
 function draw() {
   //background(0);
   //background image
-  //image(base, 0, 0, base.width/2, base.height/2);
+  
+  image(base, 0, 0, base.width/2, base.height/2);
 
   //Grid
   for (j = 0; j < squares.length; j++){
@@ -39,18 +45,41 @@ function draw() {
       y = 0;
     }
   }
-
+  if (frameCount%5==0){
   num = int(random(9));
   squares[num].filter(INVERT);
+  }
 
+  //dad
+  if (frameCount%60){
+    push();
+    //translate(random(-10, 10), 0);
+    tint(0, alp);
+    // dad.filter();
+   image(dad, 0, 0, dad.width/2, dad.height/2);
+   pop();
+   alp += g;
+  dad.filter(GRAY);
+  }
+
+  if (alp <= MIN || alp >= MAX ){
+    g *= -1;
+  }
+
+  
   //EL
   image(EL, 0, 0, EL.width/2, EL.height/2)
+  
   //flickering EL
   if (frameCount%int(random(20))==0){
     EL.filter(INVERT);
   }
 
-  
-  
-  //blend modes, filter INVERT
+  //dancing EL
+  push();
+  translate(random(-20, 20), 0);
+  //scale(-1, 1);
+  tint(random(360), 100, 100, random(40));
+  image(EL, 0, 0, EL.width/2, EL.height/2);
+  pop();
 }
